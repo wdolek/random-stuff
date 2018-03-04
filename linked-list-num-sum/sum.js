107ffc1d541ced298707874650585fc7e9bd330b
Zdenek Havlin <wdolek@hotmail.com>
2018-03-04 11:56:41 +0100

    Add readme files; Add JSDoc

--- linked-list-num-sum/sum.js
+++ linked-list-num-sum/sum.js
@@ -1,4 +1,14 @@
+/**
+ * Class representing linked-list node.
+ * @property {number} value Value of node.
+ * @property {Node}   next  Next node in chain or {null} when last node.
+ */
 class Node {
+    /**
+     * @constructor
+     * @param {number} value Node value.
+     * @param {Node}   next  Next node in list.
+     */
     constructor(value, next) {
         const intValue = Number.parseInt(value);
         if (Number.isNaN(intValue) || intValue < 0 || intValue > 9) {
@@ -9,16 +19,26 @@ class Node {
         this.next = next;
     }
 
+    /**
+     * Builds {Node} out of numeric value passed.
+     * @param {number|string} num Numeric value.
+     * @returns {Node} First node of linked-list.
+     */
     static build(num) {
         let result = null;
         for (let n of num.toString().split('').reverse()) {
-            result = new Node(Number.parseInt(n), result);
+            result = new Node(n, result);
         }
 
         return result;
     }
 }
 
+/**
+ * Creates printable value from given linked-list.
+ * @param {Node} node First linked-list node.
+ * @returns {string} String representation of node.
+ */
 function print(node) {
     var stack = buildStack(node);
 
@@ -27,6 +47,11 @@ function print(node) {
         : stack.join('');
 }
 
+/**
+ * Creates stack of values read from linked-list.
+ * @param {Node} linkedList First linked-list node.
+ * @returns {number[]} Array of linked-list values.
+ */
 function buildStack(linkedList) {
     const stack = [];
 
@@ -39,15 +64,29 @@ function buildStack(linkedList) {
     return stack;
 }
 
+/**
+ * Creates ordered array of addends by stack size, londer stack is first.
+ * @param {number[]} a First addend represented by array of digits.
+ * @param {number[]} b Second addend represented by array of digits.
+ * @returns {number[][]} Array of stacks where [0] is longer stack and [1] is shorter one.
+ */
 function sortAddendsByLength(a, b) {
     return a.length > b.length
         ? [a, b]
         : [b, a];
 }
 
+/**
+ * Sums two addends together.
+ * @param {number} a     First addend.
+ * @param {number} b     Second addend.
+ * @param {number} carry Add carry.
+ * @returns {{sum: number, carry: number}}
+ */
 function add(a, b, carry) {
     let sum = a + b + carry;
 
+    // always keep sum result in range [0..9], otherwise carry
     if (sum >= 10) {
         sum = sum - 10;
         carry = 1;
@@ -61,12 +100,23 @@ function add(a, b, carry) {
     }
 }
 
+/**
+ * Reads first value from stack.
+ * @param {number[]} stack Stack to pop value from.
+ * @returns {number} First stack value of {0} if stack is empty.
+ */
 function readStack(stack) {
     return stack && stack.length > 0
         ? stack.pop()
         : 0;
 }
 
+/**
+ * Sums two linked-lists.
+ * @param {Node} a First addend
+ * @param {Node} b Second addend
+ * @returns {Node} Sum of {a} and {b}.
+ */
 function sum(a, b) {
     let [firstAddend, secondAddend] = sortAddendsByLength(
         buildStack(a),
@@ -76,18 +126,21 @@ function sum(a, b) {
     let carry = 0;
     let result = null;
     while (firstAddend.length > 0) {
+        // pop stacks and perform sum
         const additionResult = add(
             readStack(firstAddend),
             readStack(secondAddend),
             carry
         );
 
+        // insert new node into linked-list
         result = new Node(additionResult.sum, result);
         carry = additionResult.carry;
     }
 
+    // if we carried something, add it at the start of linked-list
     if (carry > 0) {
-        result = new Node(1, result);
+        result = new Node(carry, result);
     }
 
     return result;
@@ -97,9 +150,9 @@ function sum(a, b) {
 // act
 const a = 666;
 const b = 444;
-
 const c = sum(Node.build(a), Node.build(b));
 
+// -----------------------------------------------------------------------------
 // assert
 const assert = require('assert');
 assert.equal(print(c), (a + b).toString());
--- str-reverse/reverse.js
+++ str-reverse/reverse.js
@@ -1,13 +1,17 @@
 /**
- * @param char[] chars Array of chars
- * @param number start Start index
- * @param number count Number of chars to reverse
+ * Reverse part of input array according to provided range.
+ * @param {char[]} chars Array of chars.
+ * @param {number} start Start index.
+ * @param {number} count End index.
+ * @returns {char[]}
  */
 function reverse(chars, start, end) {
-    if (end - start < 1) {
+    // range is too short for reversing
+    if (end - start <= 1) {
         return chars;
     }
 
+    // iterate just half of input
     const pivot = Math.trunc((end - start) / 2);
     for (let i = 0; i <= pivot; i++) {
         swap(chars, start + i, end - i);
@@ -17,9 +21,11 @@ function reverse(chars, start, end) {
 }
 
 /**
- * @param char[] chars   Array of chars
- * @param number idxFrom Source index
- * @param number idxTo   Destination index
+ * Swaps two fields in array (encapsulates simple swapping).
+ * @param {char[]} chars   Array of chars.
+ * @param {number} idxFrom Source index.
+ * @param {number} idxTo   Destination index.
+ * @returns {char[]}
  */
 function swap(chars, idxFrom, idxTo) {
     const temp = chars[idxFrom];
@@ -30,16 +36,21 @@ function swap(chars, idxFrom, idxTo) {
 }
 
 /**
- * @param char char Character to check
+ * Determines whether given char is white-space.
+ * @param {char} char Character to check.
+ * @returns {bool}
  */
 function isWhiteSpace(char) {
     return char == ' ' || char == "\t";
 }
 
 /**
- * @param char[] chars Array of chars
+ * Reverse words in sentence.
+ * @param {char[]} chars Array of chars.
+ * @returns {char[]}
  */
 function reverseSentence(chars) {
+    // perform reversing of words from start to last space in sentence
     let start = 0;
     for (let i = 0; i < chars.length; i++) {
         if (isWhiteSpace(chars[i])) {
@@ -48,17 +59,20 @@ function reverseSentence(chars) {
         }
     }
 
+    // reverse word from last space in sentence to the end
     reverse(chars, start, chars.length - 1);
 
     return chars;
 }
 
+// -----------------------------------------------------------------------------
 // act
 const sentence = 'this is a table'.split('');
 const expected = 'siht si a elbat';
 
 reverseSentence(sentence);
 
+// -----------------------------------------------------------------------------
 // assert
 const assert = require('assert');
 assert.equal(sentence.join(''), expected);
