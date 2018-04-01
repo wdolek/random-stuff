5b8a5e8ca580028ae62fb8c3d5d28c599e2375e4
Zdenek Havlin <wdolek@hotmail.com>
2018-04-01 23:22:34 +0200

    Remove `IsMaximizing` and `Opponent` properties from `Player` class - not really needed

--- TicTacToe/TicTacToe/Player.cs
+++ TicTacToe/TicTacToe/Player.cs
@@ -10,65 +10,21 @@ namespace TicTacToe
         /// <summary>
         /// Blank (dummy) player.
         /// </summary>
-        public static readonly Player Blank = new Player('\0', false);
-
-        /// <summary>
-        /// Player's opponent.
-        /// </summary>
-        private Player _opponent;
+        public static readonly Player Blank = new Player('\0');
 
         /// <summary>
         /// Initializes a new instance of the <see cref="Player"/> struct.
         /// </summary>
         /// <param name="mark">Player mark.</param>
-        /// <param name="isMaximizing">Whether player should maximize own score.</param>
-        public Player(char mark, bool isMaximizing)
+        public Player(char mark)
         {
             Mark = mark;
-            IsMaximizing = isMaximizing;
         }
 
         /// <summary>
         /// Gets player mark.
         /// </summary>
         public char Mark { get; }
-
-        /// <summary>
-        /// Gets a value indicating whether player should maximize own score.
-        /// </summary>
-        public bool IsMaximizing { get; }
-
-        /// <summary>
-        /// Gets or sets player's opponent.
-        /// </summary>
-        public Player Opponent
-        {
-            get => _opponent;
-            set
-            {
-                if (value == null)
-                {
-                    throw new ArgumentNullException(nameof(value));
-                }
-
-                if (ReferenceEquals(this, value))
-                {
-                    throw new ArgumentException("Cannot set self as opponent.", nameof(value));
-                }
-
-                if (Equals(value))
-                {
-                    throw new ArgumentException("Both players are equal.", nameof(value));
-                }
-
-                // save reference to opponent
-                _opponent = value;
-
-                // set opponent's opponent to self
-                // (NB! assign field directly otherwise you end up in infinite setter loop)
-                _opponent._opponent = this;
-            }
-        }
 
         /// <summary>
         /// Determines whether provided player instance is empty.
