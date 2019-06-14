919b3255fc9bce2ab9fac620b070b4848f135fae
Zdenek Havlin <wdolek@hotmail.com>
2019-06-15 00:28:06 +0200

    Add Jump Game 2 greedy algo

--- 1337Code/1337Code/JumpGame2/Jump2.cs
+++ 1337Code/1337Code/JumpGame2/Jump2.cs
@@ -34,5 +34,52 @@
 
             return steps;
         }
+
+        public int JumpGreedy(int[] nums)
+        {
+            // 1. [ 2, 3, 1, 1, 4 ]
+            //            ^-- farthest position found (-> 0 + 2 = 2)
+            //                ... continue with setting `maxPossibleRange`
+            // 2. [ 2, 3, 1, 1, 4 ]
+            //         ^-- new farthest position found! (-> 1 + 3 = 4)
+            //             ... new farthest equals max position -> we are done!
+
+            // if there's no or single number, we don't need to jump anywhere :)
+            if (nums == null || nums.Length <= 1)
+            {
+                return 0;
+            }
+
+            var numOfJumps = 0;
+            var maxPossibleRange = 0;
+            var farthestPosition = 0;
+
+            var maxPosition = nums.Length - 1;
+            for (var i = 0; i <= maxPosition; i++)
+            {
+                // try to find new farthest position
+                // (current index + max range where we can move)
+                var tryFarthest = i + nums[i];
+                if (tryFarthest > farthestPosition)
+                {
+                    farthestPosition = tryFarthest;
+                }
+
+                // reached end of array
+                if (farthestPosition >= maxPosition)
+                {
+                    return numOfJumps + 1;
+                }
+
+                // iterated to maximum possible range, we are still somewhere in the middle
+                if (i == maxPossibleRange)
+                {
+                    ++numOfJumps;
+                    maxPossibleRange = farthestPosition;
+                }
+            }
+
+            return numOfJumps;
+        }
     }
 }
