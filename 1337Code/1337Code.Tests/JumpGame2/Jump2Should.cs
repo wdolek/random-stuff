a226679733fb896e11e8ef37d8d2478d2dc8baaf
Zdenek Havlin <wdolek@hotmail.com>
2019-06-16 16:57:07 +0200

    Use `MemberData` instead of `InlineData` when testing Jump Game 2

--- 1337Code/1337Code.Tests/JumpGame2/Jump2Should.cs
+++ 1337Code/1337Code.Tests/JumpGame2/Jump2Should.cs
@@ -1,4 +1,5 @@
-﻿using _1337Code.JumpGame2;
+﻿using System.Collections.Generic;
+using _1337Code.JumpGame2;
 using Xunit;
 
 namespace _1337Code.Tests.JumpGame2
@@ -6,9 +7,7 @@ namespace _1337Code.Tests.JumpGame2
     public sealed class Jump2Should
     {
         [Theory]
-        [InlineData(new [] { 2, 3, 1, 1, 4 }, 2)]
-        [InlineData(new [] { 1, 1, 1 }, 2)]
-        [InlineData(new [] { 1 }, 0)]
+        [MemberData(nameof(GenerateTestData))]
         public void ReturnExpectedJumps(int[] nums, int expectedJumps)
         {
             var jump = new Jump2();
@@ -18,15 +17,20 @@ namespace _1337Code.Tests.JumpGame2
         }
 
         [Theory]
-        [InlineData(new[] { 2, 3, 1, 1, 4 }, 2)]
-        [InlineData(new[] { 1, 1, 1 }, 2)]
-        [InlineData(new[] { 1 }, 0)]
+        [MemberData(nameof(GenerateTestData))]
         public void ReturnExpectedGreedyJumps(int[] nums, int expectedJumps)
         {
             var jump = new Jump2();
             var result = jump.JumpGreedy(nums);
 
             Assert.Equal(expectedJumps, result);
+        }
+
+        public static IEnumerable<object[]> GenerateTestData()
+        {
+            yield return new object[] { new[] { 2, 3, 1, 1, 4 }, 2 };
+            yield return new object[] { new[] { 1, 1, 1 }, 2 };
+            yield return new object[] { new[] { 1 }, 0 };
         }
     }
 }
