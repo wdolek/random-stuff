af5b4c4077f2a65b2949fd00ddcfd9531ce7d32e
Zdenek Havlin <wdolek@hotmail.com>
2019-06-16 22:23:49 +0200

    Add `WaterContainer` problem

---
+++ 1337Code/1337Code.Tests/ContainerWithMostWatter/WaterContainerShould.cs
@@ -0,0 +1,19 @@
+﻿using _1337Code.ContainerWithMostWatter;
+using Xunit;
+
+namespace _1337Code.Tests.ContainerWithMostWatter
+{
+    public sealed class WaterContainerShould
+    {
+        [Theory]
+        [InlineData(new[] { 1, 8, 6, 2, 5, 4, 8, 3, 7 }, 49)]
+        [InlineData(new[] { 1, 1 }, 1)]
+        public void CalculateMaxArea(int[] height, int expectedArea)
+        {
+            var container = new WaterContainer();
+            var result = container.MaxArea(height);
+
+            Assert.Equal(expectedArea, result);
+        }
+    }
+}
