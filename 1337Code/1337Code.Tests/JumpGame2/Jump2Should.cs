8044189d785b44210fb8b88ec931352e58141d28
Zdenek Havlin <wdolek@hotmail.com>
2019-06-16 16:33:15 +0200

    Add Jump Game 2 tests

---
+++ 1337Code/1337Code.Tests/JumpGame2/Jump2Should.cs
@@ -0,0 +1,32 @@
+﻿using _1337Code.JumpGame2;
+using Xunit;
+
+namespace _1337Code.Tests.JumpGame2
+{
+    public sealed class Jump2Should
+    {
+        [Theory]
+        [InlineData(new [] { 2, 3, 1, 1, 4 }, 2)]
+        [InlineData(new [] { 1, 1, 1 }, 2)]
+        [InlineData(new [] { 1 }, 0)]
+        public void ReturnExpectedJumps(int[] nums, int expectedJumps)
+        {
+            var jump = new Jump2();
+            var result = jump.Jump(nums);
+
+            Assert.Equal(expectedJumps, result);
+        }
+
+        [Theory]
+        [InlineData(new[] { 2, 3, 1, 1, 4 }, 2)]
+        [InlineData(new[] { 1, 1, 1 }, 2)]
+        [InlineData(new[] { 1 }, 0)]
+        public void ReturnExpectedGreedyJumps(int[] nums, int expectedJumps)
+        {
+            var jump = new Jump2();
+            var result = jump.JumpGreedy(nums);
+
+            Assert.Equal(expectedJumps, result);
+        }
+    }
+}
