c697c9d28eb5f18a7b2c0b5a7a2eb980f907206d
Zdenek Havlin <wdolek@hotmail.com>
2019-06-21 23:12:29 +0200

    Add First missing positive number finder

---
+++ 1337Code/1337Code.Tests/FirstMissingPositive/FirstMissingPositiveFinderShould.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Generic;
+using _1337Code.FirstMissingPositive;
+using Xunit;
+
+namespace _1337Code.Tests.FirstMissingPositive
+{
+    public sealed class FirstMissingPositiveFinderShould
+    {
+        [Theory]
+        [MemberData(nameof(GenerateInput))]
+        public void FindFirstMissingPositive(int[] nums, int expectedNumber)
+        {
+            var finder = new FirstMissingPositiveFinder();
+            var result = finder.FirstMissingPositive(nums);
+
+            Assert.Equal(expectedNumber, result);
+        }
+
+        [Theory]
+        [MemberData(nameof(GenerateInput))]
+        public void FindFirstMissingPositiveWithoutExtraMemory(int[] nums, int expectedNumber)
+        {
+            var finder = new FirstMissingPositiveFinder();
+            var result = finder.FirstMissingPositiveWithoutExtraMemory(nums);
+
+            Assert.Equal(expectedNumber, result);
+        }
+
+        [Theory]
+        [MemberData(nameof(GenerateInput))]
+        public void FindFirstMissingPositiveWithMinMax(int[] nums, int expectedNumber)
+        {
+            var finder = new FirstMissingPositiveFinder();
+            var result = finder.FirstMissingPositiveWithMinMax(nums);
+
+            Assert.Equal(expectedNumber, result);
+        }
+
+        public static IEnumerable<object[]> GenerateInput()
+        {
+            yield return new object[] { new[] { 1, 2, 0 }, 3 };
+            yield return new object[] { new[] { 3, 4, -1, 1 }, 2 };
+            yield return new object[] { new[] { 7, 8, 9, 11, 12 }, 1 };
+            yield return new object[] { new[] { 1 }, 2 };
+
+            // full sequence 1..9
+            yield return new object[] { new[] { 2, 4, 6, 8, 1, 3, 5, 7, 9 }, 10 };
+
+            // no positive numbers at all
+            yield return new object[] { new[] { -1, -2, -3 }, 1 };
+        }
+    }
+}
---
+++ 1337Code/1337Code/FirstMissingPositive/FirstMissingPositiveFinder.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace _1337Code.FirstMissingPositive
+{
+    // https://leetcode.com/problems/first-missing-positive/
+    public sealed class FirstMissingPositiveFinder
+    {
+        public int FirstMissingPositive(int[] nums)
+        {
+            // turn input to set for better lookup, O(n)
+            var set = new HashSet<int>(nums);
+
+            // iterate from 1..MAX, first number not in set is our missing positive number, O(n)
+            for (var i = 1; i < int.MaxValue; i++)
+            {
+                if (!set.Contains(i))
+                {
+                    return i;
+                }
+            }
+
+            return 0;
+        }
+
+        public int FirstMissingPositiveWithoutExtraMemory(int[] nums)
+        {
+            // iterate from 1 .. (n + 1)
+            for (var i = 1; i <= nums.Length + 1; i++)
+            {
+                var numOfIs = nums.Count(v => v == i);
+                if (numOfIs == 0)
+                {
+                    return i;
+                }
+            }
+
+            return 0;
+        }
+
+        public int FirstMissingPositiveWithMinMax(int[] nums)
+        {
+            var min = int.MaxValue;
+            var max = 0;
+
+            // determine minimum and maximum values
+            for (var i = 0; i < nums.Length; i++)
+            {
+                var n = nums[i];
+
+                if (n > 0 && n < min)
+                {
+                    min = n;
+                }
+
+                if (n > max)
+                {
+                    max = n;
+                }
+            }
+
+            // we haven't found any positive number
+            if (max == 0)
+            {
+                return 1;
+            }
+
+            // if minimal positive value is [2,), first missing is just 1
+            if (min > 1)
+            {
+                return 1;
+            }
+
+            // minimal value is 1, we need to find first missing number in sequence
+            for (var n = min; n <= max; n++)
+            {
+                var found = false;
+                for (var i = 0; i < nums.Length; i++)
+                {
+                    if (nums[i] == n)
+                    {
+                        found = true;
+                        break;
+                    }
+                }
+
+                if (!found)
+                {
+                    return n;
+                }
+            }
+
+            // we haven't found missing number within sequence, so it must be max + 1
+            return max + 1;
+        }
+    }
+}
