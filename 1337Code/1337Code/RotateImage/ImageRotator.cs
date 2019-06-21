4c17f61289bcedc032204362e796f8e4e71e1b31
Zdenek Havlin <wdolek@hotmail.com>
2019-06-21 20:34:31 +0200

    Describe image rotator better; Add tests

--- 1337Code/1337Code/RotateImage/ImageRotator.cs
+++ 1337Code/1337Code/RotateImage/ImageRotator.cs
@@ -15,41 +15,55 @@
             // [8,5,2],
             // [9,6,3]
             //
-            // rotate 4 points:
-            // A > B
-            // ^   v
-            // D < C
+            // i,j -> i,j  switch
+            // ------------------
+            // 0,0 -> 0,2       Y
+            // 0,1 -> 1,2       Y
+            // 0,2 -> 2,2       Y
+            // 1,0 -> 0,1       Y
+            // 1,1 -> 1,1       Y
+            // 1,2 -> 2,1       N
+            // 2,0 -> 0,0       N
+            // 2,1 -> 1,0       N
+            // 2,2 -> 2,0       N
             //
-            // x,y    x,y  ?
-            // -------------
-            // 0,0 -> 0,2  *
-            // 0,1 -> 1,2  *
-            // 0,2 -> 2,2  *
-            // 1,0 -> 0,1  *
-            // 1,1 -> 1,1  *
-            // 1,2 -> 2,1
-            // 2,0 -> 0,0
-            // 2,1 -> 1,0
-            // 2,2 -> 2,0
+            // move all four points in single iteration
+            // ----------------------------------------
+            //
+            //   TL --> TR
+            //   ^      |
+            //   |      v
+            //   BL <-- BR
+            //
 
             var length = matrix.Length;
-            for (var i = 0; i < length / 2; i++)
+
+            // iterate half rows only
+            // (other half will be handled within processing first half)
+            for (var row = 0; row < length / 2; row++)
             {
-                for (var j = i; j < length - i - 1; j++)
+                var lastRowIndex = length - row - 1;
+
+                // iterate over less columns on further rows
+                // (processing half of the matrix, split diagonally)
+                for (var col = row; col < length - row - 1; col++)
                 {
-                    // a = [i][j];
-                    // b = [j][length - i - 1];
-                    // c = [length - i - 1][length - j - 1];
-                    // d = [length - j - 1][i];
+                    var lastColIndex = length - col - 1;
 
-                    var newX = length - i - 1;
-                    var newY = length - j - 1;
+                    // keep "top left" value
+                    var tmp = matrix[row][col];
 
-                    var tmp = matrix[i][j];
-                    matrix[i][j] = matrix[newY][i];
-                    matrix[newY][i] = matrix[newX][newY];
-                    matrix[newX][newY] = matrix[j][newX];
-                    matrix[j][newX] = tmp;
+                    // move "bottom left" to "top left"
+                    matrix[row][col] = matrix[lastColIndex][row];
+
+                    // move "bottom right" to "bottom left"
+                    matrix[lastColIndex][row] = matrix[lastRowIndex][lastColIndex];
+
+                    // move "top right" to "bottom right"
+                    matrix[lastRowIndex][lastColIndex] = matrix[col][lastRowIndex];
+
+                    // move "top left" to "top right"
+                    matrix[col][lastRowIndex] = tmp;
                 }
             }
         }
