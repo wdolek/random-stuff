ba4372b3d456b13679ff1a30a9b7752c3ca5b548
Zdenek Havlin <wdolek@hotmail.com>
2019-07-04 23:22:48 +0200

    Add k-th largest element finder (fast LINQ solution; too lazy to do Quick select sort algorithm)

---
+++ 1337Code/1337Code/LargestElementFinder/LargestElementFinder.cs
@@ -0,0 +1,13 @@
+﻿using System.Linq;
+
+namespace _1337Code.LargestElementFinder
+{
+    // https://leetcode.com/problems/kth-largest-element-in-an-array/
+    public sealed class LargestElementFinder
+    {
+        public int FindKthLargest(int[] nums, int k) =>
+            nums.OrderByDescending(v => v)
+                .Skip(k - 1)
+                .First();
+    }
+}
