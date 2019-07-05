6dc4c5d70df074351e59eb362cf918e53279c306
Zdenek Havlin <wdolek@hotmail.com>
2019-07-05 22:25:26 +0200

    Add anagram validator

---
+++ 1337Code/1337Code.Tests/ValidAnagram/AnagramValidatorShould.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using _1337Code.ValidAnagram;
+using Xunit;
+
+namespace _1337Code.Tests.ValidAnagram
+{
+    public sealed class AnagramValidatorShould
+    {
+        [Theory]
+        [MemberData(nameof(GenerateTestData))]
+        public void ValidateAnagram(string s, string t, bool isValid) {
+            var validator = new AnagramValidator();
+            var result = validator.IsAnagram(s, t);
+
+            Assert.Equal(isValid, result);
+        }
+
+        [Theory]
+        [MemberData(nameof(GenerateTestData))]
+        public void ValidateAnagramWithoutLinq(string s, string t, bool isValid)
+        {
+            var validator = new AnagramValidator();
+            var result = validator.IsAnagramWithoutLinq(s, t);
+
+            Assert.Equal(isValid, result);
+        }
+
+        public static IEnumerable<object[]> GenerateTestData()
+        {
+            yield return new object[] { "anagram", "nagaram", true };
+            yield return new object[] { "restful", "fluster", true };
+            yield return new object[] { "rat", "car", false };
+            yield return new object[] { "a", "ab", false };
+            yield return new object[] { "ab", "a", false };
+            yield return new object[] { "aa", "ab", false };
+        }
+    }
+}
---
+++ 1337Code/1337Code/ValidAnagram/AnagramValidator.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Linq;
+
+namespace _1337Code.ValidAnagram
+{
+    // https://leetcode.com/problems/valid-anagram/
+    public sealed class AnagramValidator
+    {
+        public bool IsAnagram(string s, string t)
+        {
+            if (s.Length != t.Length)
+            {
+                return false;
+            }
+
+            // create dictionary char -> num of occurrences
+            var sourceCharCount = s.GroupBy(c => c).ToDictionary(g => g.Key, g => g.Count());
+            var targetCharCount = t.GroupBy(c => c).ToDictionary(g => g.Key, g => g.Count());
+
+            // check we have same number of chars and same count of each char in both dictionaries
+            // (NB! checking size of dictionaries is not the same as checking string lengths at the beginning)
+            return sourceCharCount.Count == targetCharCount.Count
+                && sourceCharCount.All(vp => targetCharCount.ContainsKey(vp.Key) && targetCharCount[vp.Key] == vp.Value);
+        }
+
+        public bool IsAnagramWithoutLinq(string s, string t)
+        {
+            if (s.Length != t.Length)
+            {
+                return false;
+            }
+
+            // init array 0..25 to represent each char count
+            Span<int> charCount = stackalloc int['z' - 'a' + 1];
+
+            // +1 for source
+            foreach (var c in s)
+            {
+                charCount[c - 'a']++;
+            }
+
+            // -1 for target
+            foreach (var c in t)
+            {
+                charCount[c - 'a']--;
+            }
+
+            // we expect all 0
+            foreach (var i in charCount)
+            {
+                if (i != 0)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
