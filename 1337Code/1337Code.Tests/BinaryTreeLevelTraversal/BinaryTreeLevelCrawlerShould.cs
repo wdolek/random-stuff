fc10e7432248eeda7c4417795e7ae29385fd7b38
Zdenek Havlin <wdolek@hotmail.com>
2019-07-08 23:06:29 +0200

    Add binary tree level order traversal

---
+++ 1337Code/1337Code.Tests/BinaryTreeLevelTraversal/BinaryTreeLevelCrawlerShould.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using _1337Code.BinaryTreeLevelTraversal;
+using Xunit;
+using static _1337Code.BinaryTreeLevelTraversal.BinTreeLevelCrawler;
+
+namespace _1337Code.Tests.BinaryTreeLevelTraversal
+{
+    // https://leetcode.com/problems/binary-tree-level-order-traversal/
+    public sealed class BinaryTreeLevelCrawlerShould
+    {
+        [Theory]
+        [MemberData(nameof(GenerateTestData))]
+        public void ReturnCorrectValuesPerLevel(TreeNode root, IList<IList<int>> expected)
+        {
+            var crawler = new BinTreeLevelCrawler();
+            var result = crawler.LevelOrder(root);
+
+            Assert.True(expected.SequenceEqual(result, new InnerListComparer()));
+        }
+
+        public static IEnumerable<object[]> GenerateTestData()
+        {
+            var root = new TreeNode(3)
+            {
+                left = new TreeNode(9),
+                right = new TreeNode(20)
+                {
+                    left = new TreeNode(15),
+                    right = new TreeNode(7),
+                }
+            };
+
+            yield return new object[]
+            {
+                root,
+                new List<IList<int>>
+                {
+                    new List<int> { 3 },
+                    new List<int> { 9, 20 },
+                    new List<int> { 15, 7 },
+                }
+            };
+
+            yield return new object[] { (TreeNode)null, new List<IList<int>>(0) };
+        }
+
+        private class InnerListComparer : IEqualityComparer<IList<int>>
+        {
+            public bool Equals(IList<int> x, IList<int> y) => x.SequenceEqual(y);
+
+            // return `1` for any input to enforce calling of `Equals`... much clever, so hacky
+            public int GetHashCode(IList<int> obj) => 1;
+        }
+    }
+}
---
+++ 1337Code/1337Code/BinaryTreeLevelTraversal/BinTreeLevelCrawler.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections.Generic;
+
+namespace _1337Code.BinaryTreeLevelTraversal
+{
+    public sealed class BinTreeLevelCrawler
+    {
+        public IList<IList<int>> LevelOrder(TreeNode root)
+        {
+            if (root == null)
+            {
+                return new List<IList<int>>(0);
+            }
+
+            var result = new List<IList<int>>();
+
+            var queue = new Queue<(TreeNode Node, int Level)>();
+            queue.Enqueue((root, 0));
+
+            while (queue.Count > 0)
+            {
+                var (current, level) = queue.Dequeue();
+
+                // NB! we are always one item behind: list size 0 == level 0,
+                //     we need to add new entry (list) so we can push stuff into it, so level 0 -> list[0] (list size = 1)
+                if (result.Count == level)
+                {
+                    // TODO: consider pre-allocating list with its max capacity - we know max number of entries per level (2^level)
+                    //       ... yet, if tree is not balanced, we will waste precious bytes of memory, so...
+                    result.Add(new List<int>());
+                }
+
+                result[level].Add(current.val);
+
+                if (current.left != null)
+                {
+                    queue.Enqueue((current.left, level + 1));
+                }
+
+                if (current.right != null)
+                {
+                    queue.Enqueue((current.right, level + 1));
+                }
+            }
+
+            return result;
+        }
+
+        public class TreeNode
+        {
+            public int val;
+            public TreeNode left;
+            public TreeNode right;
+            public TreeNode(int x) { val = x; }
+        }
+    }
+}
